f42ee2bc6f326d1636d2a96983fbb297246811f7
Abhishek <iamabhishekfromindia@gmail.com>
2015-03-25 22:10:17 +0530

    prevented deault action on anchor click

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-$(".btn").click(function(){
+$(".btn").click(function(e){
+	e.preventDefault();
 $(".overlay").css({"visibility":"visible","opacity": "0.3"}, "slow");
 $("#nav").animate({right:'0', opacity:'1'}, "slow");
 });
-$(".close").click(function(){
+$(".close").click(function(e){
+	e.preventDefault();
     $("#nav").animate({right:'-1366px'}, "fast");
     $(".overlay").css({"visibility":"hidden","opacity": "0"}, "slow");
 });
